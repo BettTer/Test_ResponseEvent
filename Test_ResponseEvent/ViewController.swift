d6436c15a50290434f1c49aaf09116d192473b0c
yuyang.chen2 <yuyang.chen2@zhenai.com>
2020-05-07 14:51:35 +0800

    * 添加卡顿检测

--- Test_ResponseEvent/ViewController.swift
+++ Test_ResponseEvent/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import CoreFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
+    
+    var currentActivity: CFRunLoopActivity?
+    let semaphore = DispatchSemaphore.init(value: 0)
+    var runLoopObserver: CFRunLoopObserver?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +25,12 @@ class ViewController: UIViewController {
         let tapRecognizer = UITapGestureRecognizer.init(target: self, action: #selector(self.clickEvent2))
         button.addGestureRecognizer(tapRecognizer)
         
+        
+        self.decideRunningSlow()
+    }
+    
+    deinit {
+        self.removeRunLoopObserver()
     }
     
     @objc func clickEvent1() -> Void {
@@ -31,5 +43,79 @@ class ViewController: UIViewController {
         
     }
 
+}
+
+extension ViewController {
+    
+    /// Runloop检测卡顿
+    func decideRunningSlow() -> Void {
+
+        let ownInfo = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
+        
+        var runLoopObserverContext = CFRunLoopObserverContext.init(version: 0, info: ownInfo, retain: nil, release: nil, copyDescription: nil)
+                
+        runLoopObserver = CFRunLoopObserverCreate(
+            kCFAllocatorDefault,
+            CFRunLoopActivity.allActivities.rawValue,
+            true,
+            0,
+            { (observer, activity, info) in
+                
+                if observer == nil || info == nil {
+                    return
+                    
+                }
+                
+                let selfValue = Unmanaged<ViewController>.fromOpaque(info!).takeUnretainedValue()
+                selfValue.currentActivity = activity
+                selfValue.semaphore.signal()
+                
+            },
+            &runLoopObserverContext)
+        
+        CFRunLoopAddObserver(CFRunLoopGetCurrent(), runLoopObserver, CFRunLoopMode.commonModes)
+        
+        DispatchQueue.global().async {
+            
+            while (true) {
+                
+                if self.currentActivity != nil {
+                    
+                    let semaphoreWait = self.semaphore.wait(timeout: DispatchTime.now() + 0.033334)
+                    
+                    if semaphoreWait == .timedOut {
+                        
+                        switch self.currentActivity! {
+                        case .afterWaiting, .beforeSources: // * 发生了卡顿
+                            print("发生了卡顿")
+                            break
+                            
+                        default:
+                            print("正常")
+                            break
+                            
+                        }
+                        
+                    }
+                    
+                }
+            }
+        }
+        
+
+    }
+    
+    
+    func removeRunLoopObserver() {
+        if runLoopObserver == nil {
+            return
+            
+        }
+        
+        CFRunLoopRemoveObserver(CFRunLoopGetCurrent(), runLoopObserver, CFRunLoopMode.commonModes)
+
+    }
+
+    
 }
 
